7968981a65d18e81f4e502c3f4754663828e6542
Qr²o <apocaliptico@gmail.com>
2022-01-19 19:21:05 +0100

    Correlacion campo furbo

--- streamlit.py
+++ streamlit.py
@@ -2,28 +2,71 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 import os
 
-shootResults=["Todos","Goal","OwnGoal","MissedShot","BlockedShot","SavedShot","ShotOnPost"]
+shootResults=["Todos","Goal","OwnGoal","MissedShots","BlockedShot","SavedShot","ShotOnPost"]
 shootTipe=["Todos","RightFoot","LeftFoot","Head","OtherBodyPart"]
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
 
 def launchSparkGoals(jugador, tipoGol, parteCuerpo):
     os.system("python shots.py " + str(jugador) +" "+ str(tipoGol)+" "+ str(parteCuerpo))
 
 
 def scatter_plot(goles):
-
-    x = np.array(goles["positionX"] * 1.4)
-    y = np.array(goles["positionY"] * 0.9)
-
-    img = plt.imread("./images/pitch.png")
-    fig, ax = plt.subplots(facecolor="none")
-    ax.imshow(img, zorder=0, extent=[0, 1.4, 0, 0.9])
-    ax.scatter(x, y, zorder=1, color="#0086ff")
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    st.pyplot(fig)
-
+    try:
+        x = np.array(goles["positionX"] * 1.4)
+        y = np.array(goles["positionY"] * 0.9)
+        fig, ax = plt.subplots(facecolor="none")
+        ax.scatter(x, y, zorder=1, color="#0086ff")
+        img = plt.imread("./images/pitch.png")
+        ax.imshow(img, zorder=0, extent=[0, 1.4, 0, 0.9])
+        confidence_ellipse(x, y, ax, edgecolor='red')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        st.pyplot(fig)
+    except:
+        x = np.array(goles["positionX"] * 1.4)
+        y = np.array(goles["positionY"] * 0.9)
+        fig, ax = plt.subplots(facecolor="none")
+        ax.scatter(x, y, zorder=1, color="#0086ff")
+        img = plt.imread("./images/pitch.png")
+        ax.imshow(img, zorder=0, extent=[0, 1.4, 0, 0.9])
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        st.pyplot(fig)
 
 def updatePlayers():
     os.system("python start-spark.py")
